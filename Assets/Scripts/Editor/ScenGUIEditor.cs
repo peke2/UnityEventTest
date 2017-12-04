2f7ce5e75f3a817cf3c78f54fd466d60b6220f8f
Masaki Abe <abem@dc4.so-net.ne.jp>
2017-12-04 19:49:55 +0900

    シーンビューでのオブジェクト選択カスタマイズ用環境の登録。

---
+++ Assets/Scripts/Editor/ScenGUIEditor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SceneGUI))]
+public class ScenGUIEditor : Editor {
+	void OnSceneGUI()
+	{
+		Debug.Log("called["+target.name+"] = "+ Event.current.type.ToString());
+	}
+}

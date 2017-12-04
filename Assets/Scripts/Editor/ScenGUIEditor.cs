ef62c02c89d3faf58a676f935243715caa7a4d41
peke2 <abem@dc4.so-net.ne.jp>
2017-12-05 01:50:57 +0900

    オブジェクト選択をシーンビューとヒエラルキーで区別するための検証中。

--- Assets/Scripts/Editor/ScenGUIEditor.cs
+++ Assets/Scripts/Editor/ScenGUIEditor.cs
@@ -5,8 +5,56 @@ using UnityEditor;
 
 [CustomEditor(typeof(SceneGUI))]
 public class ScenGUIEditor : Editor {
-	void OnSceneGUI()
+
+    //[InitializeOnLoadMethod]
+    //void initMethod()
+    void OnEnable()
+    {
+        //OnEnable はオブジェクトが選択状態になった時にも呼び出される
+        Debug.Log("コールバック登録["+target.name+"]");
+
+        Selection.selectionChanged += selectionChanged;
+    }
+
+    private void OnDisable()
+    {
+        Selection.selectionChanged -= selectionChanged;
+    }
+
+    void selectionChanged()
+    {
+        string name = Selection.activeGameObject.name;
+        Debug.Log("選択変更=>"+name);
+    }
+
+
+    bool isFirst = true;
+    void OnSceneGUI()
 	{
-		Debug.Log("called["+target.name+"] = "+ Event.current.type.ToString());
+    /*
+        GameObject gobj = ((SceneGUI)target).gameObject;
+
+        bool isSelected = false;
+
+        if( isFirst == true )
+        {
+            Debug.Log("選択["+gobj.name+"]");
+            isFirst = false;
+
+            isSelected = true;
+        }
+
+        if( Event.current.type == EventType.Used )
+        {
+            isFirst = true;
+        }
+
+        if( isSelected == true )
+        {
+            isFirst = true;
+            Selection.activeGameObject = gobj.transform.parent.gameObject;
+        }
+        */
+		//Debug.Log("called["+target.name+"] = "+ Event.current.type.ToString());
 	}
 }
